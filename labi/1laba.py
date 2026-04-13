202f201e8fff1b482477440a4546c968bb3c07df
7hugger <lexxiy368@gmail.com>
2026-04-13 22:43:54 +0400

    Update 1laba.py

--- labi/1laba.py
+++ labi/1laba.py
@@ -1,7 +1,26 @@
-import re
+f = open("1.txt").read().split()
 
-d={'0':'ноль','1':'один','2':'два','3':'три','4':'четыре','5':'пять','6':'шесть','7':'семь','8':'восемь','9':'девять'}
-t=open("1.txt").read()
+a = []
+m = ""
 
-for n in re.findall(r'\b[0-9]{1,7}\b', t):
-    if int(n)%2==0: print(" ".join(d[c] if int(c)%2==0 and (i+1)%2==1 else c for i,c in enumerate(n)))
+for x in f:
+    ok = 1
+    for c in x:
+        if c not in "0123456789abcdefABCDEF":
+            ok = 0
+
+    if ok:
+        v = int(x, 16)
+
+        if v <= 4095 and v % 2 == 1 and len(x) > 3:
+            a.append(x)
+
+            if m == "" or v < int(m, 16):
+                m = x
+
+print(*a)
+print(len(a))
+
+for c in m:
+    print(c, end="")
+
